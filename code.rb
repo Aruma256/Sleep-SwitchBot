277943f05a6c616df8f8e3b23885283ea60e6da2
Aruma256 <22253623+Aruma256@users.noreply.github.com>
2022-01-16 09:00:46 +0900

    Clean up

--- code.rb
+++ code.rb
@@ -2,21 +2,17 @@ require "json"
 require "net/http"
 require "webrick"
 
-$headers = {
+token = ARGV[0]
+$header = {
     "Content-Type": "application/json; charset=utf8",
-    "Authorization": ARGV[0],
+    "Authorization": token,
 }
 
-$device_id = ARGV[1]
-
-srv = WEBrick::HTTPServer.new({:BindAddress => "0.0.0.0", :Port => 22559})
+device_id = ARGV[1]
+$uri = URI.parse("https://api.switch-bot.com/v1.0/devices/#{device_id}/commands")
 
 def post(command)
-    Net::HTTP.post(
-        uri = URI.parse("https://api.switch-bot.com/v1.0/devices/#{$device_id}/commands"),
-        {"command": command}.to_json,
-        $headers,
-    )
+    Net::HTTP.post($uri, {"command": command}.to_json, $header)
 end
 
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
@@ -30,6 +26,7 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
     end
 end
 
+srv = WEBrick::HTTPServer.new({BindAddress: "0.0.0.0", Port: 22559})
 srv.mount("/", MyServlet)
 trap("INT"){ srv.shutdown }
 srv.start
